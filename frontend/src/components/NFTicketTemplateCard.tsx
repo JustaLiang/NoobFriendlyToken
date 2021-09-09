de6b92bf896a79d260e1d6f29c7de7180467ee28
Justa(梁泊伸) <justa@dynacolor.com.tw>
2021-09-09 21:29:27 +0800

    Stimulate fetching image

--- frontend/src/components/NFTicketTemplateCard.tsx
+++ frontend/src/components/NFTicketTemplateCard.tsx
@@ -3,7 +3,7 @@ import { SignerContext, ProviderContext } from "../hardhat/SymfoniContext";
 import { NFTicketTemplate__factory } from "../hardhat/typechain/factories/NFTicketTemplate__factory";
 import { NFTicketTemplate } from "../hardhat/typechain/NFTicketTemplate";
 import { makeStyles } from "@material-ui/core/styles";
-import { Card, CardHeader, CardMedia, CardContent, Typography } from "@material-ui/core";
+import { Card, CardHeader, CardMedia, CardContent, Typography, Box, CircularProgress } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -28,10 +28,13 @@ interface TemplateInfo {
     maxSupply: number,
 }
 
+const baycURI = "https://bafybeihpjhkeuiq3k6nqa3fkgeigeri7iebtrsuyuey5y6vy36n345xmbi.ipfs.dweb.link/";
+
 export const NFTicketTemplateCard: React.FC<Props> = (props) => {
     const classes = useStyles();
     const signer = useContext(SignerContext);
     const provider = useContext(ProviderContext);
+    const [imageURI, setImageURI] = useState<string>("loading");
     const [template, setTemplate] = useState<NFTicketTemplate>();
     const [templateInfo, setTemplateInfo] = useState<TemplateInfo>({
         name: "name",
@@ -41,18 +44,38 @@ export const NFTicketTemplateCard: React.FC<Props> = (props) => {
     });
     const { templateAddress } = props;
     useEffect(() => {
-            if (signer[0]) {
-                setTemplate(NFTicketTemplate__factory.connect(templateAddress, signer[0]))
-                console.log("connect contract by signer")
-            }
-            else if (provider[0]) {
-                setTemplate(NFTicketTemplate__factory.connect(templateAddress, provider[0]))
-                console.log("connect contract by provider")
-            }
-            else {
-                console.log("no signer or provider")
-            }
+        if (signer[0]) {
+            setTemplate(NFTicketTemplate__factory.connect(templateAddress, signer[0]))
+            console.log("connect contract by signer")
+        }
+        else if (provider[0]) {
+            setTemplate(NFTicketTemplate__factory.connect(templateAddress, provider[0]))
+            console.log("connect contract by provider")
+        }
+        else {
+            console.log("no signer or provider")
+        }
     }, [signer, provider, templateAddress])
+
+    useEffect(() => {
+        const updateImageURI = async () => {
+            const randomNum = parseInt(templateAddress) % 10000
+            fetch(`${baycURI}${randomNum}`)
+                .then((res) => {
+                    if (res.status === 404) throw Error("URI error: 404")
+                    return res.json()
+                })
+                .then((metadata) => {
+                    setImageURI("https://ipfs.io/ipfs" + metadata.image.slice(6))
+                    console.log(randomNum)
+                })
+                .catch((err) => {
+                    console.log(err)
+                    setImageURI("")
+                })
+        }
+        updateImageURI()
+    }, [imageURI, templateAddress])
 
     useEffect(() => {
         const reloadTemplate = async () => {
@@ -71,12 +94,19 @@ export const NFTicketTemplateCard: React.FC<Props> = (props) => {
                     title={templateInfo.name}
                     subheader={templateInfo.symbol}
                 />
-                <CardMedia
-                    className={classes.media}
-                    component="img"
-                    image="https://bafybeibnzhc7vp4hnfcocw7s2jej2tj5xqpwseyz3ifylismh47cr45rhm.ipfs.dweb.link/"
-                    alt="Not initialize"
-                />
+                {imageURI !== "loading"? (
+                    <CardMedia
+                        className={classes.media}
+                        component="img"
+                        image={imageURI}
+                        alt="Not initialize"
+                    />
+                ):(
+                    <Box className={classes.media} style={{ textAlign: 'center'}} >
+                        <CircularProgress />
+                    </Box>
+                )}
+
                 <CardContent>
                     <Typography variant='h6' className={classes.info}>
                         Ticket Type: {templateInfo.ticketType}
