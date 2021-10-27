5ab9ea246f3c494f9fd372541ff7a65b530a4b43
j1998719 <j1998719@gmail.com>
2021-10-27 16:28:40 +0800

    specialMint test

--- test/Template.ts
+++ test/Template.ts
@@ -1,4 +1,5 @@
 const { ethers, deployments } = require('hardhat');
+const { expect, assert } = require("chai");
 import { utils } from "ethers";
 
 const slottingFee = utils.parseEther("0.3");
@@ -33,4 +34,50 @@ describe("Template", function () {
     const NFTBlindbox = await ethers.getContractFactory("NFTBlindbox");
     const blindbox = NFTBlindbox.attach(contractAddr);
   });
+
+  it( "blindbox should special mint", async function(){
+
+    const baseSettings = {
+      "name" : "123",
+      "symbol" : "456",
+      "payees" : [addr1.address],
+      "shares" : [1],
+      "typeOfNFT" : 1,
+      "maxSupply" : 100
+    }
+
+    await tokenAdmin.genNFTContract(baseSettings, {value: slottingFee});
+    const contractAddr= await tokenAdmin.userContracts(owner.address, 0);
+    const NFTBlindbox = await ethers.getContractFactory("NFTBlindbox");
+    const blindbox = NFTBlindbox.attach(contractAddr);
+
+    await expect(
+      blindbox.specialMint(addr1.address, 101)
+    ).to.emit(blindbox, "Transfer")
+    .withArgs(ethers.constants.AddressZero, addr1.address, 101);
+
+  });
+
+  it( "blindbox should fail special mint if tokenID == maxSupply", async function(){
+
+    const baseSettings = {
+      "name" : "123",
+      "symbol" : "456",
+      "payees" : [addr1.address],
+      "shares" : [1],
+      "typeOfNFT" : 1,
+      "maxSupply" : 100
+    }
+
+    await tokenAdmin.genNFTContract(baseSettings, {value: slottingFee});
+    const contractAddr= await tokenAdmin.userContracts(owner.address, 0);
+    const NFTBlindbox = await ethers.getContractFactory("NFTBlindbox");
+    const blindbox = NFTBlindbox.attach(contractAddr);
+
+    await expect(
+      blindbox.specialMint(addr1.address, 100)
+    ).to.be.revertedWith("special mint error");
+
+  });
+
 });
